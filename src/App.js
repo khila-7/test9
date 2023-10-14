6120dd5e7157288099c8170d05868a4466ac9a39
khila-7 <akhilasiragam@gmail.com>
2023-10-14 23:53:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Age Calculator</h1>
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
